c01991f0b8dab194aaad8a6f0709f3e8ecfe1629
William Bray <William.Bray@dvla.gsi.gov.uk>
2015-08-19 12:34:12 +0100

    Ui changes on index

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
   <!-- Optional theme -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
+  <meta name="viewport" content="width=device-width, inital-scale=1">
+
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
@@ -39,24 +41,28 @@
 
 <body>
 
-<div class="container">
+  <div class="container">
 
-  <div class="jumbotron">
-  <h1><?php echo("uberTinder")?></h1>
-  <p>Going from: <input id="startLoc" type="textbox" placeholder="SA1 3QW" class="form-control" ></p>
-  <p>Going to: <input id="startLoc" type="textbox" placeholder="SA6 7JL" class="form-control" ></p>
+    <div class="jumbotron">
+      <div class="panel panel-default">
+        <div class ="panel-body text-center">
+          <h1><?php echo("uberTinder")?></h1>
+          <p>Going from: <input id="startLoc" type="textbox" placeholder="SA1 3QW" class="form-control"</p>
+          <p>Going to: <input id="startLoc" type="textbox" placeholder="SA6 7JL" class="form-control" ></p>
+        </div>
+      </div>
 
+      <p><div id="googleMap" style="width:80%;height:50%; margin:0 auto"></div></p>
 
-  <p><div id="googleMap" style="width:80%;height:50%; margin:0 auto"></div></p>
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <p><input class="btn btn-sm btn-primary" type="button" value="Search for a Driver" id="driverSearch" style="width:calc(50% - 10px)"> <input class="btn btn-sm btn-primary" type="button" value="Search for Passengers" id="passengerSearch" style="width:calc(50% - 10px)"></p>
+        </div>
+      </div>
 
-  <p><input class="btn btn-sm btn-primary" type="button" value="Search for a Driver" id="driverSearch" style="width:calc(50% - 10px)"> <input class="btn btn-sm btn-primary" type="button" value="Search for Passengers" id="passengerSearch" style="width:calc(50% - 10px)"></p>
+    </div>
 
-
-</div>
-
-
-
-</div>
+  </div>
 
 </body>
 
