6c1066ac410658fe393060f02584afa37f9c4102
Ben Powell <blpowell@gmail.com>
2015-08-19 10:46:22 +0100

    Added DB connectivity

--- mobile/rankPassenger.php
+++ mobile/rankPassenger.php
@@ -9,6 +9,35 @@
 
 <body>
 
+  <?php
+  $username = "root";
+  $password = "";
+  $dbname = "uberTinder_DB";
+
+  $conn = new mysqli(null,
+    $username, // username
+    $password,     // password
+    $dbname,
+    null,
+    '/cloudsql/dvlahack:ubertinder'
+    );
+
+    $sql = "SELECT * FROM Users";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "UserId: " . $row["UserId"]. " - Name: " . $row["UserName"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+?>
+
+
+
 <div class="container">
   <div class="row">
       <div class="col-centered">How would you rank {name} as a passenger?</div>
